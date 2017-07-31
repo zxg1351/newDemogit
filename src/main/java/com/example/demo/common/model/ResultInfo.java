3ca1e815c3b32d590f6af566e1e4c4b08327673c
zxg1351 <zxg>
2017-07-31 21:29:08 +0800

    20170731

--- src/main/java/com/example/demo/common/model/ResultInfo.java
+++ src/main/java/com/example/demo/common/model/ResultInfo.java
@@ -8,7 +8,7 @@ import java.io.Serializable;
  * Created by Administrator on 2017/7/19.
  */
 @Data
-public class ResultInfo<T>{
+public class ResultInfo<T> implements Serializable{
 
     // 返回值Code ResultType 类型的参考
     private String resultCode;
